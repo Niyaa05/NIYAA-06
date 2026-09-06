bdd317504f4a40728ead058d148505ee6923b65b
Niyaa05 <ook756722@gmail.com>
2026-09-06 09:39:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import yfinance as yf
   def get stock_data(ticker):
-  data = yf.download(ticker, period='1mo')
-  return data
+   data = yf.download(ticker, period='1mo')
+   return data
   print(get_stock_data('RELIANCE'))
