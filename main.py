8055fa4133ae4ba181ac9ea5aa1853f1f1b960a5
Niyaa05 <ook756722@gmail.com>
2026-09-06 09:47:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import yfinance as yf
-  def get stock_data(ticker):
-   data = yf.download(ticker, period='1mo')
-   return data
-  print(get_stock_data('RELIANCE'))
+import yfinance as yf
+def get stock_data(ticker):
+data = yf.download(ticker, period='1mo')
+return data
+ print(get_stock_data('RELIANCE.NS))
